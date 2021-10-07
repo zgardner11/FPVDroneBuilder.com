553b1b4af0a5174be1a107b0a63aed3088154cb6
ZG <zgardner.cinema@gmail.com>
2021-10-07 00:18:02 -0700

    updates

---
+++ dronez.js
@@ -0,0 +1,1 @@
+function indoorDroneThree(frame,motor,fc,esc,)
---
+++ tutorials.js
@@ -0,0 +1,8 @@
+// if
+
+let inputText = myTex
+
+if (inputText === 'Yes'){
+    alert('You are correct!');
+    alert('Well done.');
+}
