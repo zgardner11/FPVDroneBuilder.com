caa7d279e28ada8b1de633790c2585ad4c6e9e84
ZG <zgardner.cinema@gmail.com>
2021-11-08 17:58:08 -0800

    cleaner layout for home and build page

--- dronez.js
+++ dronez.js
@@ -1,1 +1,8 @@
-function indoorDroneThree(frame,motor,fc,esc,)
+const navSlide = () => {
+    let toggleButton = document.getElementsByClassName('toggle-button')[0];
+    let navbarLinks = document.getElementsByClassName('navlinks')[0];
+
+    toggleButton.addEventListener('click', () => {
+        navbarLinks.classList.toggle('active');
+    });
+}
--- tutorials.js
+++ tutorials.js
@@ -1,8 +1,48 @@
 // if
 
-let inputText = myTex
+let inputText = myText()
 
 if (inputText === 'Yes'){
     alert('You are correct!');
     alert('Well done.');
 }
+
+//functions
+
+function multiplyTwo(x,y){
+    let number = x + y;
+    return number;
+}
+
+let drone = {
+    esc: 'hobbywing',
+}
+function flyDrone(drone){
+    console.log(drone.esc)
+}
+
+multiplyTwo(4,20);
+
+//objects
+
+let indoorDrone = {
+    frame:'160mm',
+    flight_controller: 25,
+    esc:null,
+    rx:'crossfire',
+    vtx:'unify nano',
+    naked_camera:true,
+    hd_camera:true
+};
+
+indoorDrone.esc; //dot notation
+indoorDrone['frame']; //bracket notation
+
+//for loop
+
+for(let z = 2; z <= 100; z++){
+    console.log(z)
+}
+
+//things to learn:
+//normalize name inputs to account for accents/other accessibility issues
